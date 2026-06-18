afea946a461f39091d3ea30c0742e652aedb8541
mt1ladmin <admin@mt1l.com>
2026-06-18 20:39:48 +0100

    Update Next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-}
+  outputFileTracingRoot: import.meta.dirname,
+};
 
-export default nextConfig
+export default nextConfig;
